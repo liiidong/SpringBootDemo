b7b013fb58ac4c591a3da9f34b4280299f2bcdce
liiidong <liiidong@outlook.com>
2019-12-22 00:40:14 +0800

    git测试merge操作

---
+++ modules/demo-modules/usermanager/src/test/java/com/enough/app/usermanager/GirMergeTest.java
@@ -0,0 +1,13 @@
+package com.enough.app.usermanager;
+
+/**
+ * @program: SpringBootDemo
+ * @description:
+ * @author: lidong
+ * @create: 2019/12/22
+ */
+public class GirMergeTest {
+    public static void main(String[] args) {
+        System.out.println("git test marge");
+    }
+}
